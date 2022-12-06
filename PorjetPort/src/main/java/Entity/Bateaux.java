dca8ab59506ccfc29b7a01e12c6d83840c023abe
kiing78 <kev97250@hotmail.fr>
2022-12-06 17:13:57 +0100

    le projet JAVA

---
+++ PorjetPort/src/main/java/Entity/Bateaux.java
@@ -0,0 +1,20 @@
+package Entity;
+
+public class Bateaux {
+	private String nom;
+	
+	public Bateaux(String nom) {
+		this.nom=nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+
+}
---
+++ PorjetPort/src/main/java/controleur/BateauxServlet.java
@@ -0,0 +1,35 @@
+package controleur;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BateauxServlet
+ */
+@WebServlet("/BateauxServlet")
+public class BateauxServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private final String VUE="/WEB-INF/Bateaux.jsp";
+       
+   
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		getServletContext().getRequestDispatcher("/Bateaux.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
